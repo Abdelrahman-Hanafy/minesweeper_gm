4214444b75a349a935524006d784ff6aaa8164f4
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2021-01-31 14:39:42 +0200

    put game strategy

---
+++ game.py
@@ -0,0 +1,10 @@
+
+
+
+def paly(size = 10,bom = 10):
+    # develop game strategy
+    # 1. creat borad andd put the bombs
+    # 2. show the board and ask for input
+    # 3. check if the location is bom
+    # 4. check if we win 
+    pass

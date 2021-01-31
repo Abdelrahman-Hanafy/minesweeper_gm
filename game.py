020e1c5d63ed4ff4dee6d76d69fc5e95e396dded
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2021-01-31 15:08:12 +0200

    add the values depend on neighbor boms

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ class Board:
         self.dug = []
         # creat the borad
         self.borad = self.make_borad()
+        # creat the numbers on each cell
+        self.values() 
 
     def make_borad(self):
         # construct the borad
@@ -25,6 +27,22 @@ class Board:
             palnted += 1
         
         return borad
+
+    def values(self):
+        # assing num 0-8 to each cell 
+        for r in range(self.size):
+            for c in range(self.size):
+                if self.borad == "*":
+                    continue
+                self.borad[r][c] = self.neighboring(r,c)
+    
+    def neighboring(self, row, col):
+        counter = 0
+        for r in range(max(0,row-1),min(self.size-1,row+2)):
+            for c in range(max(0,col-1),min(self.size,col+2)):
+                if self.borad[r][c] == '*':
+                    counter += 1
+        return counter
 
 def paly(size = 10,bom = 10):
     # develop game strategy
